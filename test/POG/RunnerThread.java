982560087b4aebaf0ab2397d0ee53ee3ecbc8dbc
Gabriel <GabrielFelipeS@users.noreply.github.com>
2024-01-20 19:59:30 -0300

    test: Test use thread

---
+++ test/POG/RunnerThread.java
@@ -0,0 +1,78 @@
+package POG;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import org.junit.jupiter.api.Assertions;
+
+public class RunnerThread {
+
+	public static void runThread(String[] params) {
+		new Thread() {
+			@Override
+			public void run() {
+				robot(params);
+			}
+		}.start();
+	}
+
+	private static void robot(String[] params) {
+		try {
+			Robot robot = new Robot();
+
+			robot.delay(500);
+
+			for (String textoParaDigitar : params) {
+				clickMouse(600, 622);
+				for (char caractere : textoParaDigitar.toCharArray()) {
+					if (caractere == '@') {
+						pressShiftAndTwo(robot);
+					} else {
+						pressKeyBy(caractere, robot);
+					}
+				}
+				robot.keyPress(KeyEvent.getExtendedKeyCodeForChar('\n'));
+			}
+			
+			robot.delay(500);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void pressShiftAndTwo(Robot robot) {
+
+		robot.keyPress(KeyEvent.VK_SHIFT);
+		robot.keyPress(KeyEvent.VK_2);
+
+		robot.delay(50);
+
+		robot.keyRelease(KeyEvent.VK_SHIFT);
+		robot.keyRelease(KeyEvent.VK_2);
+
+	}
+
+	private static void pressKeyBy(char caractere, Robot robot) {
+		robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(caractere));
+		robot.delay(50);
+		robot.keyRelease(caractere);
+	}
+
+	private static void clickMouse(int x, int y) {
+		try {
+			Robot robot = new Robot();
+			robot.delay(10);
+			robot.mouseMove(x, y);
+			robot.delay(50);
+			robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+			robot.delay(50);
+			robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+			robot.delay(50);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ test/application/RunAplicationTest.java
@@ -0,0 +1,33 @@
+package application;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+import com.br.onlineshoppingsystem.application.App;
+import com.br.onlineshoppingsystem.entities.ShoppingSystem;
+
+import POG.RunnerThread;
+
+public class RunAplicationTest {
+
+    @Test
+    public synchronized void runTest() {
+    	boolean isExecuted = true;
+    	
+    	try {
+    	//	App.main(null);  
+    		RunnerThread.runThread(new String[] {"Gabriel", "gabriel@gmail.com", "54503901", "6"});
+    		App.main(null);
+    	} catch(Exception e) {
+    		isExecuted = false;
+    	}
+    	assertTrue(isExecuted);
+    }
+    
+}
+
+
+
+
