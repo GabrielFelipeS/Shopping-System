e6215c5797eac0982c362f49a5e9ca453d3641e6
Gabriel <GabrielFelipeS@users.noreply.github.com>
2024-01-22 11:23:36 -0300

    refactor: Removendo a classe RunnerThread

--- test/application/RunAplicationTest.java
+++ test/application/RunAplicationTest.java
@@ -14,22 +14,7 @@ import com.br.onlineshoppingsystem.application.App;
 import POG.RunnerThread;
 
 public class RunAplicationTest {
-/*
-    @Test
-    public synchronized void runTest() {
-    	boolean isExecuted = true;
-    	
-    	try {
-    	//	App.main(null);  
-    		RunnerThread.runThread(new String[] {"Gabriel", "gabriel@gmail.com", "54503901", "6"});
-    		App.main(null);
-    	} catch(Exception e) {
-    		isExecuted = false;
-    	}
-    	assertTrue(isExecuted);
-    }
-  */  
-    
+
     @Test
     public void shouldTakeUserInput() {
         //InputOutput inputOutput= new InputOutput();
